c8441a2bc7645177250cfbc40db6b5d5bf480022
javierestrada <estrada.javier.525@gmail.com>
2021-09-16 13:44:56 -0500

    Initial commit

---
+++ src/main/java/com/SpringDemoThymeleaf/SpringDemoThymeleaf/PersonController.java
@@ -0,0 +1,24 @@
+package com.SpringDemoThymeleaf.SpringDemoThymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+
+@Controller
+public class PersonController {
+
+    @GetMapping
+    String getPeople(Model model) {
+        model.addAttribute("people", Arrays.asList(
+                new Person("Jaiver", 58),
+                new Person("Alesha", 42),
+                new Person("Bear", 3)
+        ));
+
+//  ----------  Here thymeleaf know to get the html file called "people"
+//  ----------  that is located in the templates directory.
+        return "people";
+    }
+}
